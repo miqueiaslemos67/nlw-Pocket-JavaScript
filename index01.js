f5d79d87833e0d0157bb9be91571e8f67f6f8b77
miqueiaslemos67 <charlescearamor@hotmail.com>
2024-09-10 18:37:40 -0300

    fundamentos 003

--- index01.js
+++ index01.js
@@ -1,17 +1,37 @@
 // arrays, objetos
 
-let metas = ["Miquéias", "36 anos."]
-console.log(metas[0] + ", " + metas[1])
+// let metas = ["Miquéias", "36 anos."]
+// console.log(metas[0] + ", " + metas[1])
+
+// let meta = {
+//     value: 'Ler um livro por mês',
+//     checked: true,
+// }
+// meta.value = "não é mais ler um livro"
+// meta.log(meta.value)     
+// console.log(metas[0] + " ," + metas[1])
+
+// function // arrow function Ex: abaixo
+
+//let criarMeta =  () => {}
+
+
+
+
+// let metas = ["Miquéias", "36 anos."]
+// console.log(metas[0] + ", " + metas[1])
 
 let meta = {
     value: 'Ler um livro por mês',
-    checked: false,
-    log: (info) => {
-        console.log(info)
-    }
-}
-meta.value = "não é mais ler um livro"
-meta.log(meta.value)     
+    checked: true,
+}   
 
-// function // arrow function Ex: abaixo
-let criarMeta =  () => {}
+let metas = [ 
+    meta,
+    {
+        value: "Academia 5 vezes por semana.",
+        checked: false,
+    }
+]
+console.log(metas[1].value)
+
